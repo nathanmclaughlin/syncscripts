be6c409985e3fea9b2a72e89deda2c7e42d0e916
Nathan McLaughlin <nmclaughlin@gmail.com>
2014-12-05 16:09:13 -0500

    Added dummy code to main module

--- dir-to-csv.py
+++ dir-to-csv.py
@@ -2,9 +2,6 @@
 
 import sys, csv, ldap, ldaphelper
 
-def main():
-    """Main entry point for the script."""
-    pass
 
 def get_student_account_info(attributes):
     """Return a list of student account attributes."""
@@ -13,6 +10,17 @@ def get_student_account_info(attributes):
 def output_to_csv(accountList):
     """Output list of accounts with attributes to CSV file"""
     pass
+
+def main():
+    """Main entry point for the script."""
+    movies = get_top_grossing_movie_links(URL)
+    with open('output.csv', 'w') as output:
+        csvwriter = csv.writer(output)
+        for title, url in movies:
+            keywords = get_keywords_for_movie(
+                'http://www.imdb.com{}keywords/'.format(url))
+            csvwriter.writerow([title, keywords])
+
 
 if __name__ == '__main__':
     sys.exit(main())
