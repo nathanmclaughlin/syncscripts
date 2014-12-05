8311e93eee555b2b9af847f40d201af6fcda125d
Nathan McLaughlin <nathan@Landfill.local>
2014-12-05 16:02:21 -0500

    Renamed file
    Updated skeleton of file. Starting over.

---
+++ dir-to-csv.py
@@ -0,0 +1,20 @@
+"""Script to export network account information in LDAP to a CSV file"""
+
+import sys, csv, ldap, ldaphelper
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+def get_student_account_info(attributes):
+    """Return a list of student account attributes."""
+    pass
+
+def output_to_csv(accountList):
+    """Output list of accounts with attributes to CSV file"""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
