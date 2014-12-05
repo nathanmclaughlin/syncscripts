a8764a7182a5249d1aa52663e11e74cd87275ae7
Nathan McLaughlin <nathan@Landfill.local>
2014-12-05 17:06:33 -0500

    First draft at this thing
    Still working on command line parameters

--- dir-to-csv.py
+++ dir-to-csv.py
@@ -2,25 +2,56 @@
 
 import sys, csv, ldap, ldaphelper
 
+"""Will get these from the command line"""
+accountType = "student"
+if accountType == "student":
+    baseDN = 'ou=StudentAccounts,o=kps'
+    retrieveAttributes = ['studentID', 'cn', 'mail'] 
+    searchFilter = '(&(objectClass=user)(studentID=*)(mail=*))'
+else:
+    baseDN = 'o=kps'
+    retrieveAttributes = ['workforceID', 'cn', 'mail'] 
+    searchFilter = '(&(objectClass=user)(workforceID=*)(mail=*))'
 
-def get_student_account_info(attributes):
-    """Return a list of student account attributes."""
+def get_account_info(accountType,baseDN,user,password):
+    """Return a list of accounts with attributes."""
+    accounts = []
+    searchScope = ldap.SCOPE_SUBTREE
+    try:
+        l = ldap.initialize('ldap://ldap1.kps')
+        l.protocol_version = ldap.VERSION3
+        l.simple_bind_s(user, password)
+        r = l.search_s(baseDN, searchScope, searchFilter, retrieveAttributes)
+        res = ldaphelper.get_search_results(r)
+        for record in res:
+            cn = record.get_attr_values('cn')[0]
+            cn = cn.lower()
+            mail = record.get_attr_values('mail')[0]
+            mail = mail.lower()
+            id = record.get_attr_values('studentID')[0]
+            accounts.append(id,cn,email)
+    except ldap.LDAPError, e:
+        print e
+    return accounts
+
+def usage():
+    """Display proper command line usage of script."""
     pass
 
-def output_to_csv(accountList):
-    """Output list of accounts with attributes to CSV file"""
-    pass
-
-def main():
+def main(argv):
     """Main entry point for the script."""
-    movies = get_top_grossing_movie_links(URL)
-    with open('output.csv', 'w') as output:
+    try:                                
+        opts, args = getopt.getopt(argv, "hg:d", ["help", "grammar="])
+    except getopt.GetoptError:           3
+        usage()                          4
+        sys.exit(2)      
+
+    accounts = get_account_info(type)
+    with open('accounts.csv', 'w') as output:
         csvwriter = csv.writer(output)
-        for title, url in movies:
-            keywords = get_keywords_for_movie(
-                'http://www.imdb.com{}keywords/'.format(url))
-            csvwriter.writerow([title, keywords])
+        for id, cn, mail in accounts:
+            csvwriter.writerow([id, cn, mail])
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
